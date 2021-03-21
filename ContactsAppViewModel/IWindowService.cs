024a2571db049c6e22bdf7b9d54508b6d0426498
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-03-21 18:04:20 +0700

    doc(IWindowService): добавлены xml-комментарии

--- ContactsAppViewModel/IWindowService.cs
+++ ContactsAppViewModel/IWindowService.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace ContactsAppViewModel
 {
+	/// <summary>
+	/// Реализует интерфейс сервиса, предоставляющего свойства 
+	/// и методы для работы с окном приложения
+	/// </summary>
 	public interface IWindowService
 	{
+		/// <summary>
+		/// Хранит результат завершения работы окна
+		/// </summary>
 		bool DialogResult { get; }
+
+		/// <summary>
+		/// Возвращает и устанавливает команду успешного закрытия окна
+		/// </summary>
 		RelayCommand OkCommand { get; set; }
+
+		/// <summary>
+		/// Возвращает и устанавливает команду закрытия окна
+		/// </summary>
 		RelayCommand CancelCommand { get; set; }
+
+		/// <summary>
+		/// Выполняет запуск окна
+		/// </summary>
+		/// <param name="dataContext"></param>
 		void ShowDialog(object dataContext);
 	}
 }
