e370c99110bd3fc9c141f7a65bdd7856cd954a81
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-03-21 17:58:22 +0700

    docs(RelayCommand): добавлены xml-комментарии

--- ContactsAppViewModel/RelayCommand.cs
+++ ContactsAppViewModel/RelayCommand.cs
@@ -9,9 +9,20 @@ namespace ContactsAppViewModel
 {
     public class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        /// <summary>
+        /// Хранит логику команды
+        /// </summary>
+        private Action<object> _execute;
 
+        /// <summary>
+        /// Хранит логику метода, реализующего проверку
+        /// команды на возвожное выполнение
+        /// </summary>
+        private Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Событие, возникающее при изменении команды
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add
@@ -24,22 +35,40 @@ namespace ContactsAppViewModel
             }
         }
 
+        /// <summary>
+        /// Инициализирует логику команду и 
+        /// метод для проверки ее включения
+        /// </summary>
+        /// <param name="execute">Метод, содержащий логику команды</param>
+        /// <param name="canExecute">Метод, содержащий логику проверки 
+        /// команды на достувность выполнения</param>
         public RelayCommand(Action<object> execute,
             Func<object, bool> canExecute = null)
         {
-            this.execute = execute;
-            this.canExecute = canExecute;
+            this._execute = execute;
+            this._canExecute = canExecute;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>Возвращает true, если команда включена 
+        /// и доступна для использования, и false, 
+        /// если команда отключена</returns>
         public bool CanExecute(object parameter)
         {
-            return this.canExecute == null ||
-                this.canExecute(parameter);
+            return this._canExecute == null ||
+                this._canExecute(parameter);
         }
 
+        /// <summary>
+        /// Запускает команду на выполнение
+        /// </summary>
+        /// <param name="parameter">Параметр команды</param>
         public void Execute(object parameter)
         {
-            this.execute(parameter);
+            this._execute(parameter);
         }
     }
 }
