de780a9a95b24ea272e5ae519651545dec8497ed
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-03-26 22:00:03 +0700

    doc(AboutWindowService): добавлены xml-комментарии

--- ContactsAppView/WindowServices/AboutWindowService.cs
+++ ContactsAppView/WindowServices/AboutWindowService.cs
@@ -8,16 +8,39 @@ using ContactsAppViewModel.Commands;
 
 namespace ContactsAppView.WindowServices
 {
+    /// <summary>
+	/// Класс <see cref="AboutWindowService"/> 
+	/// предоставляет viewModel свойства и методы для 
+	/// работы с окном <see cref="AboutWindow"/>
+	/// </summary>
 	public class AboutWindowService: IWindowService
 	{
+        /// <summary>
+        /// Запускаемое окно
+        /// </summary>
         private AboutWindow _aboutWindow;
 
+        /// <summary>
+        /// Хранит результат завершения работы окна
+        /// </summary>
         public bool DialogResult { get; }
 
+        /// <summary>
+        /// Устанавливает и возвращает команду успешного
+        /// завершения работы окна
+        /// </summary>
         public RelayCommand OkCommand { get; set; }
 
+        /// <summary>
+        /// Устанавливает и возвращает команду завершения
+        /// работы окна
+        /// </summary>
         public RelayCommand CancelCommand { get; set; }
 
+        /// <summary>
+        /// Выполняет запуск окна
+        /// </summary>
+        /// <param name="dataContext">ViewModel окна</param>
         public void ShowDialog(object dataContext)
         {
             _aboutWindow = new AboutWindow();
