7677891f4990a49970cb8e8c4601fc30b472d9fe
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-04-05 17:00:28 +0700

    doc(PhoneNumberViewModel): добавлены xml-комментарии

--- ContactsAppViewModel/ModelViewModels/PhoneNumberViewModel.cs
+++ ContactsAppViewModel/ModelViewModels/PhoneNumberViewModel.cs
@@ -7,10 +7,20 @@ using ContactsAppModel;
 
 namespace ContactsAppViewModel.ModelViewModels
 {
+	/// <summary>
+	/// /// Класс <see cref="PhoneNumberViewModel"/> организует уровень
+	/// защиты данных приложения от неккоректного ввода
+	/// </summary>
 	public class PhoneNumberViewModel: ModelViewModelBase
 	{
+		/// <summary>
+		/// Хранит изменяемый объект
+		/// </summary>
 		public PhoneNumber PhoneNumber { get; set; }
 
+		/// <summary>
+		/// Возвращает и устанавливает номер телефона
+		/// </summary>
 		public long Number
 		{
 			get
@@ -33,6 +43,11 @@ namespace ContactsAppViewModel.ModelViewModels
 			}
 		}
 
+		/// <summary>
+		/// Инифиализирует объект класса <see cref="PhoneNumberViewModel"/>
+		/// </summary>
+		/// <param name="phoneNumber">Объект класса 
+		/// <see cref="PhoneNumber"/></param>
 		public PhoneNumberViewModel(PhoneNumber phoneNumber)
 		{
 			PhoneNumber = phoneNumber;
