0c8f04297e65131d34a3d1e1a78237608ab70f74
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-04-08 18:46:21 +0700

    refactoring(ShowContactsWindow): добавлены отступы

--- ContactsAppViewModel/WindowViewModels/EditContactWindowViewModel.cs
+++ ContactsAppViewModel/WindowViewModels/EditContactWindowViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ContactsAppModel;
-using ContactsAppViewModel.Commands;
+﻿using ContactsAppViewModel.Commands;
 using ContactsAppViewModel.ModelViewModels;
 
 namespace ContactsAppViewModel.WindowViewModels
